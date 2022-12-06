4fa330cfc7c90324d0ee0f38b30df9eae435da8c
VanManh13 <vanmanh13062002@gmail.com>
2022-12-06 20:37:06 +0700

    cs2

--- script.js
+++ script.js
@@ -1,60 +1,118 @@
-const addBtn = document.querySelector("#addBtn");
-const main = document.querySelector("#main");
-addBtn.addEventListener("click", function () {
-  addNote();
-});
-const saveNotes = () => {
-  const notes = document.querySelectorAll(".note textarea");
-  console.log(notes);
-  const data = [];
-  notes.forEach((note) => {
-    data.push(note.value);
-  });
+const addBox = document.querySelector(".add-box"),
+  popupBox = document.querySelector(".popup-box"),
+  popupTitle = popupBox.querySelector("header p"),
+  closeIcon = popupBox.querySelector("header i"),
+  titleTag = popupBox.querySelector("input"),
+  descTag = popupBox.querySelector("textarea"),
+  addBtn = popupBox.querySelector("button");
 
-  if (data.length === 0) {
-    localStorage.removeItem("notes");
-  } else {
-    localStorage.setItem("notes", JSON.stringify(data));
-  }
-};
-//add note
-const addNote = (text = "") => {
-  const note = document.createElement("div");
-  note.classList.add("note");
-  note.innerHTML = `
-    <div class="tool">
-         <i class="save fas fa-save"></i>
-         <i class="trash fas fa-trash" ></i> 
-    </div>
-    <textarea>${text}</textarea>
-    `;
-  // remove note
-  note.querySelector(".trash").addEventListener("click", function () {
-    let result = confirm("Dellete this note ?");
-    if (result) {
-      note.remove();
-      saveNotes();
-    } else {
-      return;
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpdate = false,
+  updateId;
+
+addBox.addEventListener("click", () => {
+  popupTitle.innerText = "Ghi chú mới";
+  addBtn.innerText = "Thêm";
+  popupBox.classList.add("show");
+  document.querySelector("body").style.overflow = "hidden";
+  if (window.innerWidth > 660) titleTag.focus();
+});
+
+closeIcon.addEventListener("click", () => {
+  isUpdate = false;
+  titleTag.value = descTag.value = "";
+  popupBox.classList.remove("show");
+  document.querySelector("body").style.overflow = "auto";
+});
+
+function showNotes() {
+  if (!notes) return;
+  document.querySelectorAll(".note").forEach((li) => li.remove());
+  notes.forEach((note, id) => {
+    let filterDesc = note.description.replaceAll("\n", "<br/>");
+    let liTag = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${filterDesc}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="fa-regular fa-ellipsis"></i>
+                                <ul class="menu">
+                                    <li onclick="updateNote(${id}, '${note.title}', '${filterDesc}')"><i class="fa-regular fa-pen"></i>Sửa</li>
+                                    <li onclick="deleteNote(${id})"><i class="fa-regular fa-trash"></i>Xóa</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>`;
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
+}
+showNotes();
+
+function showMenu(elem) {
+  elem.parentElement.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != elem) {
+      elem.parentElement.classList.remove("show");
     }
   });
-  note.querySelector(".save").addEventListener("click", function () {
-    saveNotes();
-  });
-  note.querySelector("textarea").addEventListener("focusout", function () {
-    saveNotes();
-  });
-  main.appendChild(note);
-  saveNotes();
-};
+}
 
-(function () {
-  const lsNotes = JSON.parse(localStorage.getItem("notes"));
-  if (lsNotes === null) {
-    addNote();
-  } else {
-    lsNotes.forEach((lsNote) => {
-      addNote(lsNote);
-    });
+function deleteNote(noteId) {
+  let confirmDel = confirm("Xóa ghi chú này ?");
+  if (!confirmDel) return;
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
+}
+
+function updateNote(noteId, title, filterDesc) {
+  let description = filterDesc.replaceAll("<br/>", "\r\n");
+  updateId = noteId;
+  isUpdate = true;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = description;
+  popupTitle.innerText = "Sửa ghi chú";
+  addBtn.innerText = "Cập nhật";
+}
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let title = titleTag.value.trim(),
+    description = descTag.value.trim();
+
+  if (title || description) {
+    let currentDate = new Date(),
+      month = months[currentDate.getMonth()],
+      day = currentDate.getDate(),
+      year = currentDate.getFullYear();
+
+    let noteInfo = { title, description, date: `${month} ${day}, ${year}` };
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
+    }
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+    closeIcon.click();
   }
-})();
+});
